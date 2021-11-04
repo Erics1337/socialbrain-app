c1dadf44d54980f8c7c04ac5b3b3fd07e809ecfa
Eric <erics1337@gmail.com>
2021-11-03 22:45:50 -0600

    Added Notification Badge & Header Component Done

--- components/home/Header.js
+++ components/home/Header.js
@@ -14,38 +14,22 @@ const Header = () => {
             <View style={tw`flex-row`}>
                 <TouchableOpacity>
                     <Image source={{uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png'}} 
-                            style={tw`w-10 h-10 ml-2 objectContain`} />
+                            style={tw`w-8 h-8 ml-2 objectContain`} />
                 </TouchableOpacity>
                 <TouchableOpacity>
                     <Image source={{uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png'}} 
-                            style={tw`w-10 h-10 ml-2 objectContain`} />
+                            style={tw`w-8 h-8 ml-2 objectContain`} />
                 </TouchableOpacity>
                 <TouchableOpacity>
+                    <View style={tw`absolute z-10 text-white text-base font-sans -top-0 -right-1 w-5 h-4 bg-red-500 rounded-full flex items-center justify-center animate-pulse`}>
+                        3
+                    </View>
                     <Image source={{uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png'}} 
-                            style={tw`w-10 h-10 ml-2 objectContain`} />
+                            style={tw`w-8 h-8 ml-2 objectContain`} />
                 </TouchableOpacity>
             </View>
         </View>
     )
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        // Need to give containers a row because by default everything is column-based
-        flexDirection: 'row',
-        marginHorizontal: 20
-    },
-    iconsContainer: {
-        flexDirection: 'row'
-    },
-    logo: {
-        width: 100,
-        height: 50,
-        resizeMode: 'contain'
-    }
-})
 
 export default Header
