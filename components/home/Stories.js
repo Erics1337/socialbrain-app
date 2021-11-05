953b85394571961345c65ae61e5f2e60a5e9e954
Eric <erics1337@gmail.com>
2021-11-04 19:43:45 -0600

    Hard-coded Data

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react' 
 import { View, Text, ScrollView, Image, useColorScheme } from 'react-native'
-import tw from 'twrnc';
+import tw from 'twrnc'
 import faker from 'faker'
+import { USERS } from '../../data/users'
 
 
 const Stories = () => {
-    const [users, setUsers] = useState([])
+    // const [USERS, setUsers] = useState([])
 
-    useEffect(() => {
-        // Populates an array with 20 contextualCard objects
-        const users = [...Array(20)].map((_, i) => ({
-            ...faker.helpers.contextualCard(),
-            id: i,
-        }))
+    // useEffect(() => {
+    //     // Populates an array with 20 contextualCard objects
+    //     const USERS = [...Array(20)].map((_, i) => ({
+    //         ...faker.helpers.contextualCard(),
+    //         id: i,
+    //     }))
 
-        setUsers(users)
-    },[])
+    //     setUsers(USERS)
+    // },[])
 
     
 
     return (
         <View style={tw`mb-3`}>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                {users.map((story, index) => (
+                {USERS.map((story, index) => (
                     <View key={index} style={tw`align-center px-1`}>
                         <Image source={{ uri: story.avatar }} style={tw`h-15 w-15 rounded-full p-[1.5px] border-red-500 border-2 object-contain`} />
-                        <Text style={tw`text-white text-xs w-15 truncate text-center`}>
+                        {/* truncate tailwind class doesnt work, so I used custom JS to slice username */}
+                        <Text style={tw`text-white text-xs w-15 text-center`}>
                             {story.username.length > 9 ? story.username.slice(0, 7).toLowerCase() + '...' 
                             :
                              story.username.toLowerCase()}
---
+++ data/users.js
@@ -0,0 +1,14 @@
+export const USERS = [
+    {
+        username: 'theqazman',
+        avatar: 'https://avatars3.githubusercontent.com/u/1209898?v=3&s=460',
+    },
+    {
+        username: 'sugashaw',
+        avatar: 'https://i.imgur.com/dxjd7FB.jpg'
+    },
+    {
+        username: 'bigdaddyboi313',
+        avatar: 'https://i.imgur.com/dxjd7FB.jpg'
+    }
+]
