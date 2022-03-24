cbe8f7db77f0c71e6052ec8d4b95544d8c937a71
Eric <erics1337@gmail.com>
2022-03-23 18:29:01 -0600

    Profile Header Refactor Clean Up

--- components/profile/ProfileHeader.jsx
+++ components/profile/ProfileHeader.jsx
@@ -1,89 +1,133 @@
-import React, {useContext} from 'react'
-import tw from 'twrnc'
-import { View, Text, Image } from 'react-native';
-import UnfollowButton from './UnfollowButton';
-import FollowButton from './FollowButton';
-import DeleteButton from './DeleteButton';
-import EditableText from './EditableText';
+import React, { useContext } from "react"
+import tw from "twrnc"
+import { View, Text, Image } from "react-native"
+import UnfollowButton from "./UnfollowButton"
+import FollowButton from "./FollowButton"
+import DeleteButton from "./DeleteButton"
+import EditableText from "./EditableText"
 
-function ProfileHeader({currentUser, userData, postCount}) {
-  const {username, uid, profilePic} = userData
+function ProfileHeader({ currentUser, userData, postCount }) {
+	const { username, uid, profilePic } = userData
 
-  // console.log('currentUser', currentUser)
+	return (
+		<View style={tw`flex-row justify-around mx-2`}>
+			<ProfilePicture profilePic={profilePic} username={username} />
+			<View style={tw`flex m-3`}>
+				<ActionButton
+					currentUserUid={currentUser.uid}
+					userDataUid={userData.uid}
+					currentUser={currentUser}
+				/>
+				<UserStats
+					postCount={postCount}
+					followers={userData.followers}
+					following={userData.following}
+				/>
+				<EditableTextArea
+					currentUser={currentUser}
+					userDataSubName={userData.subName}
+					userDataBio={userData.bio}
+					userDataUsername={userData.username}
+				/>
+			</View>
+		</View>
+	)
+}
 
-  	// Anonymous function to assign value to followers w/gaurd clause to prevent error if no followers
-	const followersCount = (function () {
-		if (userData.followers.length) return userData.followers.length
-		else return 0
-	})()
+const ProfilePicture = ({ profilePic, username }) => (
+	<View style={tw`flex m-3`}>
+		<Image
+			source={{ uri: profilePic }}
+			style={tw`h-20 w-20 rounded-full p-[1.5px] border-red-500 border-2`}
+		/>
+		<Text style={tw`text-xl font-semibold w-15 text-center mx-auto`}>
+			{username}
+		</Text>
+	</View>
+)
 
-	// Get number of following
-	const followingCount = Object.values(userData?.following)
-		.map((element) => element.length)
-		.reduce((a, b) => a + b, 0)
-
+const ActionButton = ({ currentUserUid, userDataUid, currentUser }) => {
+	const userIsCurrentUser = () => currentUserUid === userDataUid
+	const userIsLoggedIn = () => currentUserUid != undefined
 	// Check if user whose page it is is being followed by currentUser in any of their groups
 	const userIsFollowed = () => {
 		if (currentUser.following == undefined) return
 		return Object.values(currentUser?.following).some((a) =>
-			a.includes(userData.uid)
+			a.includes(userDataUid)
 		)
 	}
-
-  const userIsCurrentUser = () => currentUser.uid === userData.uid
-	const userIsLoggedIn = () => currentUser.uid != undefined
-
-
-  return (
-    <View style={tw`flex-row justify-around mx-2`}>
-      <View style={tw`flex m-3`}>
-        <Image source={{ uri: profilePic }} style={tw`h-20 w-20 rounded-full p-[1.5px] border-red-500 border-2`} />
-        <Text style={tw`text-xl font-semibold w-15 text-center mx-auto`}>
-        {username}
-        </Text>
-      </View>
-      <View style={tw`flex m-3`}>
-        <View style={tw`p-1 mx-auto`}>
-          {userIsLoggedIn() &&
-                !userIsCurrentUser() ? (
-                (!userIsFollowed() ? (
-                  <FollowButton
-                    follower={currentUser.uid}
-                    followee={userData.uid}
-                  />
-                ) : (
-                    <UnfollowButton
-                      follower={currentUser.uid}
-                      followee={userData.uid}
-                    />
-                )))
-              : 
-                <DeleteButton currentUser={currentUser}/>
-              }
-        </View>
-        <View style={tw`flex-row m-auto`}>
-          <View style={tw`p-2`}>
-            <Text style={tw`mx-auto font-semibold`}>{postCount}</Text>
-            <Text>post{postCount != 1 && "s"}</Text>
-          </View>
-          <View style={tw`p-2`}>
-            <Text style={tw`mx-auto font-semibold`}>{followersCount}</Text>
-            <Text>follower{followersCount != 1 && "s"}</Text>
-          </View>
-          <View style={tw`p-2`}>
-            <Text style={tw`mx-auto font-semibold`}>{followingCount}</Text>
-            <Text>following</Text>
-          </View>
-        </View>
-        <View style={tw`p-1`}>
-          <EditableText currentUser={currentUser} inputText={userData.subName} type={'subName'} username={userData.username} />
-        </View>
-        <View style={tw`p-1`}>
-          <EditableText currentUser={currentUser} inputText={userData.bio} type={'bio'} username={userData.username} />
-        </View>
-      </View>
-    </View>
-  )
+	return (
+		<View style={tw`p-1 mx-auto`}>
+			{userIsLoggedIn() && !userIsCurrentUser() ? (
+				!userIsFollowed() ? (
+					<FollowButton
+						follower={currentUserUid}
+						followee={userDataUid}
+					/>
+				) : (
+					<UnfollowButton
+						follower={currentUserUid}
+						followee={userDataUid}
+					/>
+				)
+			) : (
+				<DeleteButton currentUser={currentUser} />
+			)}
+		</View>
+	)
 }
+
+const UserStats = ({ postCount, followers, following }) => {
+	// Assign value to followers w/gaurd clause to prevent error if no followers
+	const followersCount = followers.length ? followers.length : 0
+
+	// Get number of following
+	const followingCount = Object.values(following)
+		.map((element) => element.length)
+		.reduce((a, b) => a + b, 0)
+
+	return (
+		<View style={tw`flex-row m-auto`}>
+			<View style={tw`p-2`}>
+				<Text style={tw`mx-auto font-semibold`}>{postCount}</Text>
+				<Text>post{postCount != 1 && "s"}</Text>
+			</View>
+			<View style={tw`p-2`}>
+				<Text style={tw`mx-auto font-semibold`}>{followersCount}</Text>
+				<Text>follower{followersCount != 1 && "s"}</Text>
+			</View>
+			<View style={tw`p-2`}>
+				<Text style={tw`mx-auto font-semibold`}>{followingCount}</Text>
+				<Text>following</Text>
+			</View>
+		</View>
+	)
+}
+
+const EditableTextArea = ({
+	currentUser,
+	userDataSubName,
+	userDataBio,
+	userDataUsername,
+}) => (
+	<View>
+		<View style={tw`p-1`}>
+			<EditableText
+				currentUser={currentUser}
+				inputText={userDataSubName}
+				type={"subName"}
+				username={userDataUsername}
+			/>
+		</View>
+		<View style={tw`p-1`}>
+			<EditableText
+				currentUser={currentUser}
+				inputText={userDataBio}
+				type={"bio"}
+				username={userDataUsername}
+			/>
+		</View>
+	</View>
+)
 
 export default ProfileHeader
