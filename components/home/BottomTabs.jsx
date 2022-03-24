ebfc5199113e1c540e220e56d3a5cd7fedf66e83
Eric <erics1337@gmail.com>
2022-03-24 15:24:10 -0600

    Only Use React Native Hero Icons

--- components/home/BottomTabs.jsx
+++ components/home/BottomTabs.jsx
@@ -1,46 +1,107 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import { Divider } from 'react-native-elements/dist/divider/Divider';
-import tw from 'twrnc';
-import { auth } from '../../firebase';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faUserGroup, faSearch, faDoorClosed, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react"
+import { View, Text, TouchableOpacity, Image } from "react-native"
+import { Divider } from "react-native-elements/dist/divider/Divider"
+import tw from "twrnc"
+import { auth } from "../../firebase"
+import {
+	SearchIcon,
+	UserGroupIcon,
+	UserIcon,
+	LogoutIcon,
+} from "react-native-heroicons/outline"
+import {
+	SearchIcon as SearchIconSolid,
+	UserGroupIcon as UserGroupIconSolid,
+	UserIcon as UserIconSolid,
+} from "react-native-heroicons/solid"
 
-  
 const BottomTabs = ({ navigation }) => {
-  const [activeTab, setActiveTab] = useState('Home')
+	const [activeTab, setActiveTab] = useState("HomeScreen")
 
-    const handleSignout = async () => {
-      try {
-          await auth.signOut()
-          console.log('Signed Out Successfully')
-      } catch (error){
-          console.log(error)
-      }
-  }
+	const handleSignout = async () => {
+		try {
+			await auth.signOut()
+			console.log("Signed Out Successfully")
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-  
+	return (
+		<View style={tw`w-full bottom-[1%] z-10 pt-4`}>
+			<View style={tw`flex-row justify-evenly`}>
+				<TouchableOpacity
+					onPress={() => navigation.navigate("SearchScreen")}>
+					<SearchIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+				</TouchableOpacity>
+				<Divider width={1} orientation='vertical' />
+				<TouchableOpacity
+					onPress={() => navigation.navigate("GroupsScreen")}>
+					<UserGroupIcon
+						color='black'
+						style={tw`w-7 h-7 ml-2 my-1`}
+					/>
+				</TouchableOpacity>
+				<Divider width={1} orientation='vertical' />
+				<TouchableOpacity
+					onPress={() =>
+						navigation.navigate("ProfileScreen", {
+							uid: auth.currentUser.uid,
+						})
+					}>
+					<UserIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+				</TouchableOpacity>
+				<Divider width={1} orientation='vertical' />
+				<TouchableOpacity onPress={() => handleSignout()}>
+					<LogoutIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+				</TouchableOpacity>
+			</View>
+		</View>
+	)
+}
 
-    return (
-      <View style={tw`w-full bottom-[1%] z-10`}>
-        <Divider width={1} orientation='vertical' />
-          <View style={tw`flex-row justify-evenly pt-4`}>
-            <TouchableOpacity onPress={() => navigation.push('SearchScreen')}>
-              <FontAwesomeIcon icon={ faSearch } style={tw`w-7 h-7 ml-2 my-1`} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setActiveTab('groups')}>
-              <FontAwesomeIcon icon={ faUserGroup } style={tw`w-7 h-7 ml-2 my-1`} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('ProfileScreen', {uid: auth.currentUser.uid})}>
-              <FontAwesomeIcon icon={ faUser } style={tw`w-7 h-7 ml-2 my-1`} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSignout()}>
-              <FontAwesomeIcon icon={ faArrowRightFromBracket } style={tw`w-7 h-7 ml-2 my-1`} />
-            </TouchableOpacity>
-          </View>
-      </View>
-    )
+const IconTabs = (handleSignout) => {
+	const handlePress = (linkTo) => {
+		// setActiveTab(linkTo)
+		navigation.navigate(linkTo, { uid: auth.currentUser.uid })
+	}
+	return (
+		<View>
+			{activeTab == "SearchScreen" ? (
+				<SearchIconSolid color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+			) : (
+				<TouchableOpacity onPress={() => handlePress("SearchScreen")}>
+					<SearchIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+				</TouchableOpacity>
+			)}
+			<Divider width={1} orientation='vertical' />
+			{activeTab == "GroupsScreen" ? (
+				<UserGroupIconSolid
+					color='black'
+					style={tw`w-7 h-7 ml-2 my-1`}
+				/>
+			) : (
+				<TouchableOpacity onPress={() => handlePress("GroupsScreen")}>
+					<UserGroupIcon
+						color='black'
+						style={tw`w-7 h-7 ml-2 my-1`}
+					/>
+				</TouchableOpacity>
+			)}
+			<Divider width={1} orientation='vertical' />
+			{activeTab == "ProfileScreen" ? (
+				<UserIconSolid color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+			) : (
+				<TouchableOpacity onPress={() => handlePress("ProfileScreen")}>
+					<UserIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+				</TouchableOpacity>
+			)}
+			<Divider width={1} orientation='vertical' />
+			<TouchableOpacity onPress={() => handleSignout()}>
+				<LogoutIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
+			</TouchableOpacity>
+		</View>
+	)
 }
 
 export default BottomTabs
--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import tw from 'twrnc';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faSquarePlus, faMessage, faHeart } from '@fortawesome/free-regular-svg-icons'
-
+import { 	
+	ChatIcon,
+	HeartIcon,
+	PlusCircleIcon, } from "react-native-heroicons/outline";
 const Navbar = ({ navigation }) => {
     return (
-        <View style={tw`flex-row justify-between mx-2`}>
+        <View style={tw`flex-row justify-between mx-2 items-center`}>
             <TouchableOpacity onPress={() => navigation.push('HomeScreen')} >
                 <Image style={tw`w-60 h-11 my-1`} source={require('../../assets/logoText.png')} />
             </TouchableOpacity>
 
             <View style={tw`flex-row`}>
                 <TouchableOpacity onPress={() => navigation.push('ActivityScreen')}>
-                    <FontAwesomeIcon icon={faHeart} style={tw`w-7 h-7 ml-2 my-1`} />
+                    <HeartIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigation.push('NewPostScreen')}>
-                    <FontAwesomeIcon icon={faSquarePlus} style={tw`w-7 h-7 ml-2 my-1`} />
+                    <PlusCircleIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.push('Messaging')}>
+                <TouchableOpacity onPress={() => navigation.push('MessagingScreen')}>
                 <View style={tw`absolute z-10 text-white text-base font-sans -top-0 -right-1 w-5 h-4 bg-red-500 rounded-full flex items-center justify-center`}>
                         <Text>3</Text>
                     </View>
-                    <FontAwesomeIcon icon={faMessage} style={tw`w-7 h-7 ml-2 my-1`} />
+                    <ChatIcon color='black' style={tw`w-7 h-7 ml-2 my-1`} />
                 </TouchableOpacity>
                 
             </View>
