9c475a9e85f219a496525b93497fabb2121d2978
Eric <erics1337@gmail.com>
2022-03-25 13:03:44 -0600

    Formatting w/ prettier

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,37 +1,34 @@
-import React, { useEffect, useState, useContext } from 'react'
-import SafeAreaView from 'react-native-safe-area-view';
+import React, { useEffect, useState, useContext } from "react"
+import SafeAreaView from "react-native-safe-area-view"
 
-import Navbar from '../components/home/Navbar';
-import Stories from '../components/home/Stories';
-import Posts from '../components/home/posts/Posts';
-import BottomTabs from '../components/home/BottomTabs';
-import UserContext from '../context/userContext';
-import Loader from '../components/Loader';
+import Navbar from "../components/home/Navbar"
+import Stories from "../components/home/Stories"
+import Posts from "../components/home/posts/Posts"
+import BottomTabs from "../components/home/BottomTabs"
+import UserContext from "../context/userContext"
+import Loader from "../components/Loader"
 import tw from "twrnc"
 import { auth } from "../firebase"
-import GroupsTabs from '../components/GroupsTabs';
-
-
+import GroupsTabs from "../components/GroupsTabs"
 
 const HomeScreen = ({ navigation }) => {
-    const {loginUser, loading} = useContext(UserContext);
+	const { loginUser, loading } = useContext(UserContext)
 
-    useEffect(() => {
+	useEffect(() => {
 		if (auth.currentUser) loginUser()
 		// else navigation.push('LoginScreen')
-    }, [])
+	}, [])
 
 	if (loading) return <Loader />
-    return (
-        <SafeAreaView style={tw`flex-1`}>
-            <Navbar navigation={navigation} />
-            <GroupsTabs />
-            <Stories navigation={navigation} linksTo={'ProfileScreen'} />
-            <Posts navigation={navigation}/>
-            <BottomTabs navigation={navigation} />
-        </SafeAreaView>
-    )
+	return (
+		<SafeAreaView style={tw`flex-1`}>
+			<Navbar navigation={navigation} />
+			<GroupsTabs />
+			<Stories navigation={navigation} linksTo={"ProfileScreen"} />
+			<Posts navigation={navigation} />
+			<BottomTabs navigation={navigation} />
+		</SafeAreaView>
+	)
 }
-
 
 export default HomeScreen
